f30ebd68775bca1b2276d035693b440ed2bba744
Kabongwe-Manelisi <118818827+Kabongwe-Manelisi@users.noreply.github.com>
2023-11-01 21:21:11 +0200

    user input prompts

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-
+#welcome banner
 heading =  '-' * 5 + ' FINANCIAL VISUALIZER ' + '-' * 5
 line = '-' * len(heading)
 print (line + '\n' + heading + '\n' + line)
+
+#user input prompts
+salary = input('Annual Salary:\n')
+housing = input('Monthly Housing:\n')
+bills = input('Monthly Bills:\n')
+food = input('Weekly Food:\n')
+travel = input('Annual Travel:\n')
