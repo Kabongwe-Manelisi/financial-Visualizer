8a67e8819635d7f544b4b700c789701dfd55ff87
Kabongwe-Manelisi <118818827+Kabongwe-Manelisi@users.noreply.github.com>
2023-11-02 13:52:26 +0200

    validating inputs

--- app.py
+++ app.py
@@ -9,3 +9,9 @@ housing = input('Monthly Housing:\n')
 bills = input('Monthly Bills:\n')
 food = input('Weekly Food:\n')
 travel = input('Annual Travel:\n')
+
+#validating inputs
+if salary.isnumeric() and housing.isnumeric() and bills.isnumeric() and food.isnumeric() and travel.isnumeric():
+    print ('inputs confirmed valid')
+else :
+    print('invalid input, please try again')
